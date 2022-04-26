ba575946ed4aeca93556670419aa1079dc088294
Romil Shah <romilshah2199@gmail.com>
2022-04-26 09:50:25 +0530

    find, filter

--- cypress/integration/my-tests/selector-example.test.js
+++ cypress/integration/my-tests/selector-example.test.js
@@ -9,18 +9,35 @@ describe('selectors-examples', () => {
         cy.visit('https://www.saucedemo.com/');
     })
 
-    it('should test GET', () => {
+    it('get method', () => {
         cy.get('input#user-name').type(username);
         cy.get('input#password').type(password);
 
         cy.get('input[data-test="login-button"]').click();
     });
 
-    it('first|eq|last', () => {
+    it('first|eq|last method', () => {
         cy.get('input').first().type(username);
 
         cy.get('input').eq(1).type(password);
 
         cy.get('input').last().click();
+    })
+
+
+    it('filter method', () => {
+        cy.get('input').filter('[type="text"]').type(username);
+        
+        cy.get('input').filter('#password').type(password);
+
+        cy.get('input').filter('.submit-button').click();
+    })
+
+    it('find method', () => {
+        cy.get('form').find('input').filter('#user-name').type(username);
+        
+        cy.get('form').find('input').filter('#password').type(password);
+        
+        cy.get('form').find('input').filter('.submit-button').click();
     })
 })
